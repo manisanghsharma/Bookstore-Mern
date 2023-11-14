72a5ad750348cfba0250afccb0e3f58011f6112d
Manisangh Sharma <manisanghsharma@gmail.com>
2023-11-14 22:55:06 +0530

    Made the app responsive

--- frontend/src/pages/EditBook.jsx
+++ frontend/src/pages/EditBook.jsx
@@ -57,7 +57,7 @@ const EditBook = () => {
       {loading ? (
         <Spinner />
       ) : (
-        <div className="p-5 border-2 border-sky-800 rounded-xl w-[600px] mx-auto max-md:w-[400px]">
+        <div className="p-5 border-2 border-sky-800 rounded-xl w-[600px] mx-auto max-md:w-[350px]">
           <form
             onSubmit={(e) => {
               e.preventDefault();
